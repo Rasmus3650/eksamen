e5b102a11a661dac3a1d31c729f7b366c5dc46e7
vonlohmann <williamlohmann@live.dk>
2019-04-02 13:53:40 +0200

    forbedret

--- algo/bubblesort.py
+++ algo/bubblesort.py
@@ -1,25 +1,22 @@
 import random
 import numpy as np
 
+arr = np.random.randint(1, 1000, size=int(input()))
+
 def bubbleSort(arr):
     n = len(arr)
     arr=arr
     counter = 0
 
-    for i in range(n-1):
+    while True:
+        for i in range(n-1):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
             else:
                 counter += 1
-    if counter == len(arr)-1:
-        print(arr)
-        exit
-    else:
-        counter = 0
-        bubbleSort(arr)
+        if counter == len(arr)-1:
+            return arr
+        else:
+            counter = 0
 
-arr = []
-for _ in range(int(input())):
-    arr.append(random.randint(0,1000))
-
-bubbleSort(arr)
+print(bubbleSort(arr))
