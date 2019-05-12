5047e3f19d8a85fe29f0492cd963bfae7e68fe8a
vonlohmann <williamlohmann@live.dk>
2019-05-12 15:56:01 +0200

    Clipboard og open file virker mere eller mindre

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 import os
 import random
+import pyperclip
 import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import *
 from eksamen.algo.mergesort import Mergesort
 from eksamen.algo.bubblesort import Bubblesort
@@ -35,6 +37,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.actionExit.triggered.connect(self.exit)
         self.mergeSortButton.clicked.connect(self.mergesortarray)
         self.bubbleSortButton.clicked.connect(self.bubblesortarray)
+        self.copyClipboardButton.clicked.connect(self.copyclipboard)
+        self.openFileButton.clicked.connect(self.openfilearray)
 
     def radiohide(self):
         if self.radioRandom.isChecked():
@@ -57,6 +61,46 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def bubblesortarray(self):
         Bubblesort(self.array)
 
+    def copyclipboard(self):
+        self.converttoarray(str(pyperclip.paste()), 1)
+
+    def openfilearray(self):
+        file = QFileDialog.Options()
+        file |= QFileDialog.DontUseNativeDialog
+        filename, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*);;Json Files (*.json);;Text Files (*.txt);;CSV Files (*.csv)", options=file)
+        fil = open(filename, "r")
+        filecontent = fil.read()
+        self.converttoarray(filecontent, 2)
+
+    def converttoarray(self, userinput, tal):
+        clipboard = userinput
+        clipstr = ""
+        cliparray = []
+        if clipboard[0] == "[":
+            clipboard = clipboard[1::]
+        if clipboard[-1] == "]":
+            clipboard = clipboard[:-1:]
+
+        try:
+            for i in range(len(clipboard)):
+                if clipboard[i] != ",":
+                    clipstr += clipboard[i]
+                    int(clipstr)
+                else:
+                    cliparray.append(int(clipstr))
+                    clipstr = ""
+
+            if len(cliparray) != 0:
+                self.array = cliparray
+                if tal == 1:
+                    self.randomizedLabel.setText("Copied From Clipboard")
+                elif tal == 2:
+                    self.randomizedLabel.setText("Copied From File")
+            else:
+                self.randomizedLabel.setText("Not A Valid Array")
+        except:
+            self.randomizedLabel.setText("Not A Valid Array")
+
     def exit(self):
         exit()
 
@@ -66,3 +110,5 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+#[5, 3, 2, 1, 7, 4, 8]
