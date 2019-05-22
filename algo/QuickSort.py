555dad81d7d494d81d64432c5b2b0db03b7ba878
rune2112 <rune2112@gmail.com>
2019-05-22 18:18:38 +0200

    QuickSort kommentare

--- algo/QuickSort.py
+++ algo/QuickSort.py
@@ -40,13 +40,7 @@ class Quicksort():
             self.start = pivot + 1
             #Kører sig selv med en ny start-variabel
             self.start_fun()
-            """
-            for i in range(len(self.array) - 1):
-                plt.plot(x=i + 1, ymin=0, ymax=self.array[i])
-                print(i)
-            plt.axis([0, len(self.array) + 1, 0, self.array[-1]])
-            plt.show()
-            """
+
 
     def partition(self,array,start,end):
         #Vi starter med at lave to variable x og i, og de er afhængige af de parametre denne funktion tager imod
