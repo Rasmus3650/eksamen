b8aeed82fee9cdc7b2b7844994fac86cad07e1cc
rune2112 <rune2112@gmail.com>
2019-05-22 18:27:30 +0200

    NN21 mystery flower

--- algo/NeuralNetwork21.py
+++ algo/NeuralNetwork21.py
@@ -110,6 +110,7 @@ class NeuralNetwork21(object):
     def start(self, iter, data):
         print("start")
         costs, w1, w2, b = self.train(iter, data)
-        print("Mystery: " + str(self.sigmoid((self.mystery_flower[0] * w1) + (self.mystery_flower[1] * w2) + b)))
+        mystery_pred = self.sigmoid((self.mystery_flower[0] * w1) + (self.mystery_flower[1] * w2) + b)
+        print("Mystery: " + str(mystery_pred))
         fig = plt.plot(costs)
         plt.show(fig)
