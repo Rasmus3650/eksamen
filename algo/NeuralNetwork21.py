2916d413639d74afd7c1383ff753c3c058c4eaf9
Rune Jørgensen <rune2112@gmail.com>
2019-05-23 09:03:51 +0200

    NN21 kommentare

--- algo/NeuralNetwork21.py
+++ algo/NeuralNetwork21.py
@@ -4,7 +4,7 @@ import numpy as np
 class NeuralNetwork21(object):
     def __init__(self, iter):
         self.iter = iter
-        # each point is length, width, type (0, 1)
+        #hvert punkt er længde, bredde og type (0, 1)
         self.data = [[3, 1.5, 1],
                      [2, 1, 0],
                      [4, 1.5, 1],
@@ -18,66 +18,46 @@ class NeuralNetwork21(object):
         self.start(self.iter, self.data)
     # network
 
-    #       o  flower type
+    #       o  blomster type
     #      / \  w1, w2, b
-    #     o   o  length, width
+    #     o   o  længde, bredde
 
 
-
-
-    def vis_data(self,data):
-        plt.grid()
-
-        for i in range(len(data)):
-            c = 'r'
-            if data[i][2] == 0:
-                c = 'b'
-            #plt.scatter([data[i][0]], [data[i][1]], c=c)
-
-        #plt.scatter([mystery_flower[0]], [mystery_flower[1]], c='gray')
 
 
     # activation function
-
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
 
     def sigmoid_p(self, x):
         return self.sigmoid(x) * (1-self.sigmoid(x))
 
-    X = np.linspace(-5, 5, 100)
-
-    #plt.plot(X, sigmoid(X), c="b") # sigmoid in blue
-    #fig1 = plt.plot(X, sigmoid_p(X), c="r") # sigmoid_p in red
-
-
 
     # train
-
     def train(self, iter, data):
-        # random init of weights
+        # Vi starter ud med tilfældige vægte og bias
         w1 = np.random.randn()
         w2 = np.random.randn()
         b = np.random.randn()
 
         iterations = iter
         learning_rate = 0.2
-        costs = []  # keep costs during training, see if they go down
+        costs = []  # holder styr på cost, så vi kan se om den falder
 
         for i in range(iterations):
-            # get a random point
+            # får et tilfældigt punkt
             ri = np.random.randint(len(data))
             point = data[ri]
 
+            # Regner vores netværks gæt ud, ud fra det tilfældige punkt
             z = point[0] * w1 + point[1] * w2 + b
             pred = self.sigmoid(z)  # networks prediction
 
+            #Dette er det rigtige svar til punktet
             target = point[2]
 
-            # cost for current random point
-            cost = np.square(pred - target)
 
-            # print the cost over all data points every 1k iters
+            # print the cost over all data points every 100 iters
             if i % 100 == 0:
                 c = 0
                 for j in range(len(data)):
@@ -85,10 +65,11 @@ class NeuralNetwork21(object):
                     p_pred = self.sigmoid(w1 * p[0] + w2 * p[1] + b)
                     c += np.square(p_pred - p[2])
                 costs.append(c)
-                #print(cost)
 
+            # Regner cost ud ud fra den afledte cost-funktion med den afledte sigmoid
             dcost_dpred = 2 * (pred - target)
             dpred_dz = self.sigmoid_p(z)
+
 
             dz_dw1 = point[0]
             dz_dw2 = point[1]
@@ -100,6 +81,7 @@ class NeuralNetwork21(object):
             dcost_dw2 = dcost_dz * dz_dw2
             dcost_db = dcost_dz * dz_db
 
+            #Ændrer på vægte og bias for at optimere netværket
             w1 = w1 - learning_rate * dcost_dw1
             w2 = w2 - learning_rate * dcost_dw2
             b = b - learning_rate * dcost_db
@@ -108,8 +90,13 @@ class NeuralNetwork21(object):
 
 
     def start(self, iter, data):
+        # Kører vores train funktion
         costs, w1, w2, b = self.train(iter, data)
+
+        #Finder ud af hvilken type blomst, vores netværk vil sige "mysteryflower" er
         mystery_pred = self.sigmoid((self.mystery_flower[0] * w1) + (self.mystery_flower[1] * w2) + b)
         print("Mystery: " + str(mystery_pred))
+
+        #Plotter vores cost, for at se om den falder
         fig = plt.plot(costs)
         plt.show(fig)
